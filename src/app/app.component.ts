005e35fd2512ba1246523d2e86d1b1b3cecec9da
unknown <shameemkhanam@gmail.com>
2022-12-27 23:51:28 +0530

    fragment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CoursesService } from './Services/courses.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [CoursesService]
+  providers: [CoursesService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'AngularRouting';
 
+  constructor(private activatedRoute: ActivatedRoute) {}
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnInit() {
+    this.activatedRoute.fragment.subscribe((value) => {
+      console.log(value);
+      this.jumpTo(value as string);
+    });
+  }
+
+  jumpTo(section: string) {
+    document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+  }
 }
